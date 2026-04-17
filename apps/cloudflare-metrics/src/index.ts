c33675c83ecee6817a55fc27c1d1a70ce1d01ac0
Zack <zack@futo.org>
2026-04-17 16:25:02 +0100

    chore: trigger redeploy to test usage_model regression

--- apps/cloudflare-metrics/src/index.ts
+++ apps/cloudflare-metrics/src/index.ts
@@ -1,6 +1,8 @@
 import { handleFetch } from './handlers/http.js';
 import { handleScheduled } from './handlers/scheduled.js';
 
+export const BUILD_MARKER = 'usage-model-regression-test-1';
+
 export default {
   fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     return handleFetch(request, env, ctx);
