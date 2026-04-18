1aeb1e1608f0ee39a9b8409c76a5952740cc96e9
Zack <zack@futo.org>
2026-04-18 01:09:46 +0100

    test: force cloudflare-metrics redeploy to validate revert pattern
    previous deploy (ea725556) at 21:58 UTC reverted to bundled at 23:06 UTC
    (~1h8m post-deploy). bump FORCE_NEW_VERSION to trigger a new version and
    confirm both (a) redeploy fixes the 50ms cap immediately, and (b) the
    ~1 hour revert pattern repeats on the new version.

--- apps/cloudflare-metrics/src/index.ts
+++ apps/cloudflare-metrics/src/index.ts
@@ -1,7 +1,7 @@
 import { handleFetch } from './handlers/http.js';
 import { handleScheduled } from './handlers/scheduled.js';
 
-export const FORCE_NEW_VERSION = 1;
+export const FORCE_NEW_VERSION = 2;
 
 export default {
   fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
